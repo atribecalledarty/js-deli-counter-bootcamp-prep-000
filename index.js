12cf9bea54e8163ba729750efff4628eef428578
Arthur Song <arth3rs0ng@gmail.com>
2019-03-29 02:46:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function takeANumber(currLine, newPerson){
 function nowServing(currLine){
   if (currLine.length > 0){
     var currPerson = currLine.shift()
-    return 'Currently serving ' + currPerson
+    return 'Currently serving ' + currPerson + '.'
   } else {
     return 'There is nobody waiting to be served!'
   }
