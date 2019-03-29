1006b26450bc9448d8e5bc3600a03a9aeeec65a4
Arthur Song <arth3rs0ng@gmail.com>
2019-03-29 02:42:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function takeANumber(currLine, newPerson){
   currLine.push(newPerson)
-  console.log('Welcome, ' + newPerson + '. You are number '+ currLine.length + ' in line.')
+  console.log('Welcome, ' + newPerson + '. You are number '+ (currLine.length + 1) + ' in line.')
   
 }
