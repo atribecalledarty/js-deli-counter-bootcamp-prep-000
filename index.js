ae70e6e52693ae7275bd4100693b7f11013a83a7
Arthur Song <arth3rs0ng@gmail.com>
2019-03-29 02:43:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(currLine, newPerson){
   currLine.push(newPerson)
-  return 'Welcome, ' + newPerson + '. You are number '+ (currLine.length + 1) + ' in line.'
+  return 'Welcome, ' + newPerson + '. You are number '+ currLine.length + ' in line.'
   
 }
 
